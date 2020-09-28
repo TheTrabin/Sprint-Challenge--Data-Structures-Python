0e847d375a8d0c00a2fcca7ca13a38cafe387822
TheTrabin <beyondtears@gmail.com>
2020-09-28 01:43:59 +0000

    Completed ring buffer. began analyzing names

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,30 @@
+#Initialize Queue, []
+#storage = queue
+#No need to reset Queue head or tail, does it on it's own
+#Resets directory location to 0
+#Tail and head can point to the same location. Queue is empty = Size = 0
+#Head and tail can be greater than each other. Same pointers, allowed to cross.
+
+#after running tests, 0 can't be current location. Noting the stylization of the tests,
+# it sort of has to be behind in order to append, like schedules of most arrays.
+# -1 seems to function, as if outside of the array.
+
+# Head and Tail aren't used, but left as reference.
+
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.capacity = capacity
+        # self.head = 0
+        # self.tail = 0
+        self.pointer = -1
+        self.storage = []
 
     def append(self, item):
-        pass
+        if len(self.storage) == self.capacity:
+            self.pointer = (self.pointer + 1) % self.capacity
+            self.storage[self.pointer] = item
+        else:
+            self.storage.append(item)
 
     def get(self):
-        pass
+        return self.storage
